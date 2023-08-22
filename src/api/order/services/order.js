29755262e29443353bfc5364c52251c96c3f89a8
adamspicedev <adam@spicey.dev>
2023-08-23 10:41:13 +1200

    handled stock updates

--- src/api/order/services/order.js
+++ src/api/order/services/order.js
@@ -1,9 +1,39 @@
-'use strict';
+"use strict";
 
 /**
  * order service
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::order.order');
+module.exports = createCoreService("api::order.order", ({ strapi }) => ({
+  async create({ data }) {
+    data.products.forEach(async (product) => {
+      const serverProduct = await strapi.entityService.findOne(
+        "api::product.product",
+        product.productId,
+        {
+          fields: ["stock"],
+        }
+      );
+
+      await strapi.entityService.update(
+        "api::product.product",
+        product.productId,
+        {
+          data: {
+            stock: serverProduct.stock - product.quantity,
+          },
+        }
+      );
+    });
+
+    return await strapi.entityService.create("api::order.order", {
+      data: {
+        name: data.name,
+        email: data.email,
+        products: data.products,
+      },
+    });
+  },
+}));
